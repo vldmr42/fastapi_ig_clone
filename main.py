a34d9079a0fb72f1a16868a10964c462a1754956
manemejeff <kmb14056@yandex.ru>
2021-12-29 22:25:00 +0400

    tags, summary, description

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 
 app = FastAPI()
 
@@ -16,13 +16,29 @@ def index():
 #     return {'message': 'All blogs provided'}
 
 
-@app.get('/blog/all')
+@app.get('/blog/all',
+         tags=['blog'],
+         summary='Retrieve all blogs',
+         description='This api call simulates fetching all blogs',
+         response_description='The list of available blogs'
+         )
 def get_all_blogs(page=1, page_size: Optional[int] = None):
     return {'message': f'All {page_size} blogs on page {page}'}
 
-@app.get('/blog/{id}/comments/{comment_id}')
+
+@app.get('/blog/{id}/comments/{comment_id}', tags=['blog', 'comment'])
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulates retrieving a comment of a blog
+
+    - **id**: mandatory path parameter
+    - **comment_id**: mandatory path parameter
+    - **valid**: optional query parameter
+    - **username**: optional query parameter
+
+    """
     return {'message': f'{id=} {comment_id=} {valid=} {username=}'}
+
 
 class BlogType(str, Enum):
     short = 'short'
@@ -30,10 +46,16 @@ class BlogType(str, Enum):
     howto = 'howto'
 
 
-@app.get('/blog/type/{blog_type}')
+@app.get('/blog/type/{blog_type}', tags=['blog'])
 def get_blog_type(blog_type: BlogType):
     return {'message': f'Blog type {blog_type}'}
 
-@app.get('/blog/{id}')
-def get_blog(id: int):
-    return {'message': f'Blog with id {id}'}
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog {id} not found'}
+    else:
+        response.status_code = status.HTTP_200_OK
+        return {'message': f'Blog with id {id}'}
