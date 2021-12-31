6d276f533d146c56527b7639499cfe44857e5489
manemejeff <kmb14056@yandex.ru>
2021-12-31 23:03:01 +0400

    create User

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI
 
 from db import models
 from db.database import engine
-from router import blog_get, blog_post
+from router import blog_get, blog_post, user
 
 app = FastAPI()
+app.include_router(user.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
