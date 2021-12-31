c95a3c5e0f40fb7a6e0720c19b6d2d3ab662f089
manemejeff <kmb14056@yandex.ru>
2021-12-31 11:31:43 +0400

    Path, request and body parameters, parameters metadata.

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,10 +1,42 @@
-from fastapi import APIRouter
+from typing import Optional
+
+from fastapi import APIRouter, Query
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix='/blog',
     tags=['blog'],
 )
 
-@router.post('/new')
-def create_blog():
-    pass
+
+class BlogModel(BaseModel):
+    title: str
+    content: str
+    published: Optional[bool]
+    nb_comments: int
+
+
+@router.post('/new/{id}')
+def create_blog(blog: BlogModel, id: int, version: int = 1):
+    return {
+        'data': blog,
+        'id': id,
+        'version': version,
+    }
+
+
+@router.post('/new/{id}/comment')
+def create_comment(blog: BlogModel,
+                   id: int,
+                   comment_id: int = Query(None,
+                                           title='id of the comment',
+                                           description='Description of comment',
+                                           alias='commentId',
+                                           deprecated=True,
+                                           )
+                   ):
+    return {
+        'blog': blog,
+        'id': id,
+        'comment_id': comment_id,
+    }
