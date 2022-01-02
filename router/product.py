6570ebd2fd820366b4870c8e7b8f432044be79b2
manemejeff <kmb14056@yandex.ru>
2022-01-02 20:48:41 +0400

    Cookies

--- router/product.py
+++ router/product.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Header
+from fastapi import APIRouter, Depends, HTTPException, status, Header, Cookie
 from fastapi.responses import Response, HTMLResponse, PlainTextResponse
 from sqlalchemy.orm import Session
 
@@ -17,16 +17,24 @@ products = ['watch', 'camera', 'phone']
 @router.get('/all')
 def get_all_products():
     data = ' '.join(products)
-    return Response(content=data, media_type='text/plain')
+    response = Response(content=data, media_type='text/plain')
+    response.set_cookie(key='test_cookie', value='test_cookie_value')
+    return response
 
 
 @router.get('/withheader')
 def get_product(
         response: Response,
-        custom_header: Optional[List[str]] = Header(None)
+        custom_header: Optional[List[str]] = Header(None),
+        test_cookie: Optional[str] = Cookie(None)
 ):
-    response.headers['custom_response_header'] = ' '.join(custom_header)
-    return products
+    if custom_header:
+        response.headers['custom_response_header'] = ' '.join(custom_header)
+    return {
+        'data': products,
+        'custom_header': custom_header,
+        'my_cookie': test_cookie,
+    }
 
 @router.get('/{id}', responses={
     200: {
